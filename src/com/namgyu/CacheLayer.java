121ae6661d0bc3e205c1b79c8e43a5fb0ed48b4d
Namgyu Ho <itsnamgyu@gmail.com>
2018-12-14 12:54:46 +0900

    basic interfaces
    + (high latency) database interface
    + caching layer for performance boost

---
+++ src/com/namgyu/CacheLayer.java
@@ -0,0 +1,40 @@
+package com.namgyu;
+
+public abstract class CacheLayer<K, V> implements Database<K, V> {
+    /*
+    A caching layer to provide fast read/write access to a database.
+
+    Rationale: why abstract class?
+    There may be different ways to implement a caching layer between the user
+    and the underlying database (origin). Such a caching layer would be
+    beneficial if access to origin is very slow.
+     */
+
+    /*
+    The original database (whose access performance we want to enhance).
+     */
+    Database<K, V> origin;
+
+    public CacheLayer(Database<K, V> origin) {
+        this.origin = origin;
+    }
+
+    /*
+    Get value from some caching mechanism. If it is not available, this would require
+    access from the original database.
+     */
+    public abstract V get_value(K key);
+
+    /*
+    Set value of some value in the database. The immediate flag requires that the
+    original database be updated immediately. Otherwise, a lazy update method may be
+    used where the value is only updated within the cache and written back to the
+    database afterwards.
+     */
+    public abstract void set_value(K key, boolean immediate);
+
+    /*
+    Perform all lazy updates (mentioned in set_value) immediately.
+     */
+    public abstract void flush();
+}
---
+++ src/com/namgyu/Database.java
@@ -0,0 +1,16 @@
+package com.namgyu;
+
+public interface Database<K, V> {
+    /*
+    A database interface the provides read/write access via key-value pairs.
+    Can be used along with a CacheLayer to enhance performance of high-latency
+    mediums such as hard drive disk access, network access, DB queries etc.
+
+    Ideally, this would extend the Map Collection, but I think that's outside the scope
+    of this small project.
+     */
+
+    V get_value(K key);
+
+    void put_value(K key);
+}
