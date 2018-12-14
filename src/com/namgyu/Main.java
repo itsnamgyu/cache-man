5438e8f600d8509c69eef868ce364f98cc2f2ad9
Namgyu Ho <itsnamgyu@gmail.com>
2018-12-14 17:44:57 +0900

    add test code

--- src/com/namgyu/Main.java
+++ src/com/namgyu/Main.java
@@ -1,29 +1,53 @@
 package com.namgyu;
 
-import java.io.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws ZenFileStore.NotEnoughZenException {
+        long start;
+        long elapsed;
+        int delay = 500;
 
-        writer.write("Hello World\n");
-        writer.write("For now, I'll just repeat what you say until you say stop.\n");
-        writer.flush();
+        System.err.println("Make sure to run with assertions enabled");
 
-        while (true) {
-            writer.write("> ");
-            writer.flush();
-            String s = reader.readLine();
-            if (s.equals("stop")) {
-                writer.write("Goodbye...\n");
-                writer.flush();
-                return;
-            } else {
-                writer.write(s);
-                writer.newLine();
-                writer.flush();
-            }
-        }
+        // Non-cached version
+        // 500 ms per access
+        System.out.println("Setting access delay of " + delay + "ms");
+        System.out.println();
+        ZenFileStore db = new ZenFileStore(delay);
+        db.reset();
+
+        System.out.println("Testing without cache...");
+        start = System.currentTimeMillis();
+        mockAccess(db);
+        elapsed = System.currentTimeMillis() - start;
+        System.out.println("Time elapsed: " + elapsed + "ms");
+        System.out.println();
+
+
+        // Cached Version
+        db.reset();
+        HashCacher<String, String> cached = new HashCacher<>(db);
+
+        System.out.println("Testing with cache");
+        start = System.currentTimeMillis();
+        mockAccess(cached);
+        cached.flush();
+        elapsed = System.currentTimeMillis() - start;
+        System.out.println("Time elapsed: " + elapsed + "ms");
+        System.out.println();
+    }
+
+    private static void mockAccess(Database<String, String> db) {
+        db.set_value("a", "Hello World!");
+        assert(db.get_value("a").equals("Hello World!"));
+        assert(db.get_value("b") == null);
+        db.set_value("b", "Alohamora!");
+        assert(db.get_value("c") == null);
+        assert(db.get_value("b").equals("Alohamora!"));
+        assert(db.get_value("a").equals("Hello World!"));
+        assert(db.get_value("c") == null);
+        db.set_value("b", "Wingardium Leviosa!");
+        assert(db.get_value("b").equals("Wingardium Leviosa!"));
+        assert(db.get_value("a").equals("Hello World!"));
+        assert(db.get_value("b").equals("Wingardium Leviosa!"));
     }
 }
